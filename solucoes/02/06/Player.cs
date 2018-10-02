68bcaa94ec2418547aba811fc6615154d60d5664
André Santos <afbsantos1@gmail.com>
2018-10-02 14:46:56 +0100

    solucion ex06

---
+++ solucoes/02/06/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Player
+    {
+        //Variável de instância só de leitura
+        private string name { get; }
+
+        private double hp;
+
+        //Variável de classe só de leitura
+        public static double VidaMaxima { get; } = 100;
+
+        public Player(string name, double hp)
+        {
+            this.name = name;
+            this.hp = hp;
+
+        }
+
+        //Propriedade de classe só de leitura
+        public string GetName()
+        {
+            return name;
+        }
+
+    }
+}
---
+++ solucoes/02/06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria o jogador indicando o nome e hp
+            Player jogador1 = new Player("Joao", 100);
+
+            //Testa o programa
+            Console.WriteLine("Player 1: " + jogador1.GetName());
+        }
+    }
+}
