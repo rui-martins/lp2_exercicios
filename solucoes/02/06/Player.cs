a9834656bed62ff7e9a3bd5f46126461c3f84aec
André Santos <afbsantos1@gmail.com>
2018-10-03 20:50:50 +0100

    corrections

--- solucoes/02/06/Player.cs
+++ solucoes/02/06/Player.cs
@@ -1,29 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace _03
 {
+    /// <summary>
+    /// Classe Player: 
+    /// </summary>
     class Player
     {
-        //Variável de instância só de leitura
-        private string name { get; }
 
+        /// <summary>
+        /// Criação de variáveis de instância.
+        /// Definem a informação contida numa classe. 
+        /// São declaradas no corpo da classe, mas fora de qualquer método,
+        /// estando acessíveis a todos os métodos da classe.
+        /// Sendo hp e name variáveis de instância "normal", currentScore
+        /// trata-se de uma variável de instância só de leitura.
+        /// </summary>
         private double hp;
+        private string name;
+        readonly int currentScore;
 
-        //Variável de classe só de leitura
+        //confirmar ?? 
+        /// <summary>
+        /// Propriedade (de instância) só de leitura.
+        /// Fornece a possibilidade de ler, graver ou calcular algo, neste caso
+        /// sendo apenas de leitura iremos obter apenas o score do jogador.
+        /// </summary>
+        public int Score { get { return currentScore; } }
+
+        /// <summary>
+        /// Propriedade de classe só de leitura.
+        /// O jogador só pode ter hp máximo de 100, sendo uma propriedade de 
+        /// classe só de leitura, não permite ser alterado o hp máximo.
+        /// </summary>
         public static double VidaMaxima { get; } = 100;
 
+
+        /// <summary>
+        /// Constructor que inicializa as variáveis relevantes.
+        /// </summary>
+        /// <param name="name">Indica o nome do jogador</param>
+        /// <param name="hp">é escolhido o hp do jogador sendo o 
+        /// máximo 100</param>
         public Player(string name, double hp)
         {
             this.name = name;
             this.hp = hp;
-
         }
 
-        //Propriedade de classe só de leitura
+        /// <summary>
+        /// Método apenas para testar o programa na class Program.cs
+        /// </summary>
+        /// <returns></returns>
         public string GetName()
         {
             return name;
