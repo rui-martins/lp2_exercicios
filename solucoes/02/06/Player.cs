f9dcfba77e8718f7258feb5708d905e734b6bf9c
André Santos <afbsantos1@gmail.com>
2018-10-05 14:20:18 +0100

    change comments and fix things

--- solucoes/02/06/Player.cs
+++ solucoes/02/06/Player.cs
@@ -9,28 +9,22 @@ namespace _03
 
         /// <summary>
         /// Criação de variáveis de instância.
-        /// Definem a informação contida numa classe. 
-        /// São declaradas no corpo da classe, mas fora de qualquer método,
-        /// estando acessíveis a todos os métodos da classe.
-        /// Sendo hp e name variáveis de instância "normal", currentScore
-        /// trata-se de uma variável de instância só de leitura.
         /// </summary>
-        private double hp;
-        private string name;
-        readonly int currentScore;
+        readonly private double hp;
+        readonly private string name;
 
-        //confirmar ?? 
+        /// <summary>
+        /// Criação de variável de classe só de leitura
+        /// </summary>
+        private static int numberOfWeapons = 2;
+
         /// <summary>
         /// Propriedade (de instância) só de leitura.
-        /// Fornece a possibilidade de ler, graver ou calcular algo, neste caso
-        /// sendo apenas de leitura iremos obter apenas o score do jogador.
         /// </summary>
-        public int Score { get { return currentScore; } }
+        public int Score { get; }
 
         /// <summary>
         /// Propriedade de classe só de leitura.
-        /// O jogador só pode ter hp máximo de 100, sendo uma propriedade de 
-        /// classe só de leitura, não permite ser alterado o hp máximo.
         /// </summary>
         public static double VidaMaxima { get; } = 100;
 
